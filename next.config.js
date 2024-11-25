52b203e6657c6e5b4dd23e2e46e71e0520eb80c4
juji <him@jujiyangasli.com>
2024-11-25 13:50:56 +0700

    use next14\

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@
 // const nextConfig: NextConfig = {
 //   /* config options here */
 // };
+// export default nextConfig;
 
 
 const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig
